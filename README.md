c3643f7f659b4591f67c397e6e3a9201900ae020
Jisan-Mia <jisanmia47@gmail.com>
2020-11-16 00:25:07 +0600

    computer component filter app using javascript

---
+++ README.md
@@ -0,0 +1,1 @@
+# computer-part-filter-js
---
+++ script.js
@@ -0,0 +1,74 @@
+
+
+const filterBtns = document.querySelectorAll('.filter-btn-div .btn');
+const component = document.querySelectorAll('.component');
+
+/*
+// This is the first and small solution
+filterBtns.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		e.preventDefault();
+		const filter = e.target.dataset.filter;
+		console.log(filter)
+		
+		component.forEach((item) => {
+			if(filter == 'all'){
+				item.style.display = 'block';
+			} else{
+				if(item.classList.contains(filter)){
+					item.style.display = 'block';
+				}
+				else{
+					item.style.display = 'none';
+				}
+			}
+		})
+
+	})
+})
+*/
+
+
+
+// this is the second  solution
+
+function displayFilteredItem(className){
+	component.forEach(item => {
+		if(item.classList.contains(className)){
+			item.style.display = 'block';
+		} else {
+			item.style.display = 'none';
+		}
+	})
+}
+
+filterBtns.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		const filter = e.target.dataset.filter;
+
+		if(filter == 'all'){
+			component.forEach(item => {
+				item.style.display = 'block';
+			})
+		} else if (filter == 'cpu'){
+			displayFilteredItem('cpu')
+		} else if (filter == 'memory'){
+			displayFilteredItem('memory');
+		} else if (filter == 'os') {
+			displayFilteredItem('os');
+		} else if (filter == 'input-device'){
+			displayFilteredItem('input-device');
+		} else if (filter == 'ouput-device') {
+			displayFilteredItem('ouput-device');
+		} else if (filter == 'software') {
+			displayFilteredItem('software');
+		} else if (filter == 'mobo'){
+			displayFilteredItem('mobo');
+		} else if (filter == 'others') {
+			displayFilteredItem('others');
+		}
+
+	})
+})
